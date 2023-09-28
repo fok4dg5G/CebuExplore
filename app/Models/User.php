5f96f2aad33dc50fc0911001e9dc3208ae6fc122
satoshiYS <142573874+satoshiYS@users.noreply.github.com>
2023-09-28 15:27:33 +0800

    ポストマイグレーションファイルの作成
    bookmarkテーブルの作成・追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name','email','password',
     ];
 
     /**
@@ -29,8 +27,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password','remember_token',
     ];
 
     /**
