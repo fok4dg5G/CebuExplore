eb5a0c5dd7ca3d272315a45574f2e74615066988
satoshiYS <142573874+satoshiYS@users.noreply.github.com>
2023-09-28 15:29:23 +0800

    ブックマークテーブル作成
    ブックマークテーブルの編集済み

--- database/migrations/2023_09_28_072626_create_bookmarks_table.php
+++ database/migrations/2023_09_28_072626_create_bookmarks_table.php
@@ -15,6 +15,10 @@ class CreateBookmarksTable extends Migration
     {
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
             $table->timestamps();
         });
     }
