41850a887cb08ad24b0e66667bb901da326a6e54
amio628 <shibaooolyk@gmail.com>
2023-10-13 13:07:04 +0800

    最新
    修正しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 {{------------------------------------------------------------------------- --}}
 <div class="content">
-    <h1 class="title bb">CEBU <br>EXPLORE</h1>
-    <p class="bb">Let's share new information <br>about Cebu Island</p>
+    {{-- <h1 class="title bb">CEBU <br>EXPLORE</h1>
+    <p class="bb">Let's share new information <br>about Cebu Island</p> --}}
     @if (Route::has('login'))
         <div class="btn-container">
             @auth
@@ -17,9 +17,9 @@
                 </form>
             @else
                 <!-- ユーザーがログインしていない場合 -->
-                <a href="{{ route('login') }}" class="btn">Log in</a>
+                <a href="{{ route('login') }}" class="btn1">Login</a>
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn">Register</a>
+                    <a href="{{ route('register') }}" class="btn1">Register</a>
                 @endif
             @endauth
         </div>
@@ -32,10 +32,45 @@
 </div>
 {{-- ------------------------------------------------------------------------ --}}
 <div class="low"> 
-    <a href="{{ route('tourist-spot') }}" class="btn_low">#TouristSpost</a>
+    <div>
+        <a href="{{ route('tourist-spot') }}" class="btn_low">#TouristSpost</a>
+        <div class="low2">
+            <img src="img/yahho.png" alt="" width="80px" height="60px"> 
+            <img src="img/turtlept3.jpg" alt="" width="80px" height="60px">
+        </div>
+    </div>
+
+  <div>
     <a href="{{ route('hotel') }}" class="btn_low">#Hotel</a>
+    <div class="low2">
+        <img src="img/image34.jpg" alt=""width="80px" height="60px"> 
+        <img src="img/hotel.jpg" alt="" width="80px" height="60px">
+    </div>
+  </div>
+    
+  <div>
     <a href="{{ route('food') }}" class="btn_low">#Food</a>
+    <div class="low2">
+        <img src="img/rechon.jpg" alt=""width="80px" height="60px"> 
+        <img src="img/ハロハロ.jpg" alt="" width="80px" height="60px">
+    </div>
+  </div>
+
+  <div>
     <a href="{{ route('transportation') }}" class="btn_low">#Transportation</a>
+    <div class="low2">
+        <img src="img/gypny.jpg" alt=""width="80px" height="60px"> 
+        <img src="img/bic.jpg" alt="" width="80px" height="60px">
+    </div>
+  </div>
+   
+
 </div>
+
+ 
+   
+
+
+
 {{-- ------------------------------------------------------------------------------------- --}}
 @endsection
